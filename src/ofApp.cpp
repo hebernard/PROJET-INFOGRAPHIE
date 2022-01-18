bb3ee5791ba40f963e05d04b37ca9535780cd4a7
Jonathan Roy-Noël <jonathan.roynoel@gmail.com>
2022-01-18 17:44:59 -0500

    added simple image import and custom console window

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,20 +1,69 @@
 #include "ofApp.h"
-#include "ofxDatGui.h";
 
-//--------------------------------------------------------------
-void ofApp::setup(){
-	ofxDatGui* myGui = new ofxDatGui(ofxDatGuiAnchor::TOP_RIGHT);
-	myGui->setTheme(new ofxDatGuiThemeCharcoal());
+void ofApp::setup()
+{
+	csl = new console("Console");
+
+	ofxDatGui* import = new ofxDatGui(ofxDatGuiAnchor::TOP_LEFT);
+	ofxDatGuiFolder* importFolder = import->addFolder("Importer", ofColor::yellow);
+	ofxDatGuiButton* importButton = importFolder->addButton("Importer une image");
+	importButton->onButtonEvent(this, &ofApp::onImportButtonClicked);
+}
+
+void ofApp::onImportButtonClicked(ofxDatGuiButtonEvent e)
+{
+	ofFileDialogResult openFileResult = ofSystemLoadDialog("Choisir une image JPG ou PNG");
+
+	if (openFileResult.bSuccess)
+	{
+		processOpenFileSelection(openFileResult);
+	}
+	else
+	{
+		csl->log("Une erreur est survenue, veillez reessayer", ofLogLevel::OF_LOG_ERROR);
+	}
+}
+
+void ofApp::processOpenFileSelection(ofFileDialogResult openFileResult)
+{
+	ofFile file(openFileResult.getPath());
+
+	if (file.exists())
+	{
+		string fileExtension = ofToUpper(file.getExtension());
+
+		if (fileExtension == "JPG" || fileExtension == "PNG")
+		{
+			image.load(openFileResult.getPath());
+			csl->log("Load " + file.getFileName());
+			if (image.getWidth() > ofGetWidth() || image.getHeight() > ofGetHeight())
+			{
+				image.resize(image.getWidth() / 2, image.getHeight() / 2);
+			}
+		}
+		else
+		{
+			csl->log("Mauvais format, veillez choisir JPG ou PNG", ofLogLevel::OF_LOG_ERROR);
+		}
+	}
+
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
-
+void ofApp::update()
+{
+	csl->update();
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw()
+{
+	if (image.isAllocated())
+	{
+		image.draw(0, 0);
+	}
 
+	csl->draw();
 }
 
 //--------------------------------------------------------------
