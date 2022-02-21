b6b86e0ec633ff3311ae88c85364cf53a5f5fee8
Jonathan Roy-Noël <jonathan.roynoel@gmail.com>
2022-02-21 16:46:42 -0500

    fixed square

--- src/components/2d/square.cpp
+++ src/components/2d/square.cpp
@@ -12,15 +12,14 @@ void square::customDraw()
 	if (noFill) { ofNoFill(); }
 	else { ofFill(); }
 
-	ofDrawRectangle(0, 0, 0, m_size, m_size);
+	ofDrawRectangle(-m_size/2, -m_size / 2, 0, m_size, m_size);
 
 	ofPopStyle();
 }
 
 glm::vec3 square::getCenter()
 {
-	glm::vec3 position(getPosition().x + m_size/2, getPosition().y + m_size/2, getPosition().z);
-	return position;
+	return getPosition();
 }
 
 glm::vec3 square::getBBox()
