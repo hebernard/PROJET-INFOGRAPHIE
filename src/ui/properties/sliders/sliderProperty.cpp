0bd508dfac0bd5e46643d41180f3a60cfa213676
Jonathan Roy-Noël <jonathan.roynoel@gmail.com>
2022-04-12 19:24:39 -0400

    Update sliderProperty.cpp

--- src/ui/properties/sliders/sliderProperty.cpp
+++ src/ui/properties/sliders/sliderProperty.cpp
@@ -3,12 +3,12 @@
 
 sliderProperty::sliderProperty(std::string label, float& ref, float min, float max, bool manualUpdate) : slider(new floatSliderProperty(label, ref, min, max, manualUpdate))
 {
-	dynamic_cast<floatSliderProperty&>(*slider).onUpdate = onUpdate;
+	dynamic_cast<floatSliderProperty&>(*slider).onUpdate = [&](auto v) { onUpdate(v); };
 }
 
 sliderProperty::sliderProperty(std::string label, int& ref, int min, int max, bool manualUpdate) : slider(new intSliderProperty(label, ref, min, max, manualUpdate))
 {
-	dynamic_cast<intSliderProperty&>(*slider).onUpdate = onUpdate;
+	dynamic_cast<intSliderProperty&>(*slider).onUpdate = [&](auto v) { onUpdate(v); };
 }
 
 sliderProperty::~sliderProperty()
